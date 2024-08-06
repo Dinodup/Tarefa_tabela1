4b73c6b5e962b7ab76c07149f2d59f23f1ce308f
Dinodup <dudup2626@gmail.com>
2024-08-06 12:27:42 -0300

    comuita

--- Modificações/alteraPessoa.php
+++ Modificações/alteraPessoa.php
@@ -1,7 +1,7 @@
 <?php
     include('../Includes/conexao.php');
     $id = $_GET['id'];
-    $sql = "SELECT * FROM pessoa WHERE id=$id";
+    $sql = "SELECT * FROM pessoa WHERE id = $id";
     $result = mysqli_query($con,$sql);
     $row = mysqli_fetch_array($result);
 ?>
@@ -12,63 +12,52 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../Styles/style1.css">
+    <link rel="stylesheet" href="../Styles/style2.css">
 </head>
 <body>
-    <form action="../Modificações/alteraCidadeExe.php" method="post">
-    <fieldset>
-        <legend>Cadastro de Pessoa</legend>
-        <div>
-            <label for="nome">Nome: </label>
-            <input type="text" name="nome" id="nome"
-                    value="<?php echo $row['nome']?>">
-        </div>
-        <div>
-            <label for="email">Email: </label>
-            <input type="text" name="email" id="email"
-                    value="<?php echo $row['email']?>">
-        </div>
-        <div>
-            <label for="endereco">Endereço: </label>
-            <input type="text" name="endereco" id="endereco"
-                    value="<?php echo $row['endereco']?>">
-        </div>
-        <div>
-            <label for="bairro">Bairro: </label>
-            <input type="text" name="bairro" id="bairro"
-                    value="<?php echo $row['bairro']?>">
-        </div>
-
-        <div>
-            <label for="id_cidade">Id da Cidade</label>
-            <select name="id_cidade" id="id_cidade">
-                <?php
-                include('../Includes/conexao.php');
-                $sql = "SELECT * FROM cidade";
-                $result = mysqli_query($con,$sql);
-                while($row = mysqli_fetch_array($result)){
-                    echo "<option value='".$row['id']."'>"
-                         .$row['nome_cidade']."/".$row['estado']
-                         ."</option>";
-                }
-                ?>
-            </select>
-        </div>
-
-        <div>
-            <label for="cep">CEP: </label>
-            <input type="number" name="cep" id="cep"
-                    value="<?php echo $row['cep']?>">
-        </div>
-
-        <div>
-            <input type="hidden" name="id" value="<?php echo $row['id']?>">
-        </div>
-        <div>
-            <button type="submit">Alterar</button>
-        </div>
-    </fieldset>
-    </form>
-    <a href="ListarCidade.php">Voltar</a>
+    <main>
+            <aside class="form-flex">
+            <div class="form-container">
+                <form class="form" action="alteraPessoaExe.php" method="post">
+                    <div">
+                        <label for="nome">Nome</label>
+                        <input type="text" name="nome" id="surname">
+                    </div>
+                    <div>
+                        <label for="email">Email da Pessoa</label>
+                        <input type="text" name="email" id="surname">
+                    </div>
+                    <div>
+                        <label for="endereco">Endereço da Pessoa</label>
+                        <input type="text" name="endereco" id="surname">
+                    </div>
+                    <div>
+                        <label for="bairro">Bairro da Pessoa</label>
+                        <input type="text" name="bairro" id="bairro">
+                    </div>
+                    <input type="hidden" name="id" value="<?php echo $row['id']?>">
+                    <label for="id_cidade">Cidade:<br></label>
+                    <div class="input-group">
+                        <select name="id_cidade" id="id_cidade">
+                            <?php
+                                include('../Includes/conexao.php');
+                                $sql = "SELECT * FROM cidade";
+                                $result = mysqli_query($con,$sql);
+                                while($row = mysqli_fetch_array($result))
+                                {
+                                    echo "<option value='".$row['id']."'>".$row['nome_cidade']."/".$row['estado']."</option>";
+                                }
+                            ?>
+                        </select>
+                    </div>
+                    <div>
+                        <label for="cep">CEP</label>
+                        <input type="number" name="cep" id="cep">
+                    </div>
+                    <input class="sign" type="submit" value="Cadastrar">
+                </form>
+            </div>
+        </aside>
+    </main>
 </body>
 </html>
--- Modificações/alteraPessoaExe.php
+++ Modificações/alteraPessoaExe.php
@@ -1,48 +1,24 @@
 <?php
-    include('../Includes/conexao.php');
-    $id = $_POST['id'];
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $endereco = $_POST['endereco'];
-    $bairro = $_POST['bairro'];
-    $id_cidade = $_POST['id_cidade'];
-    $cep = $_POST['cep'];
+        include('../Includes/conexao.php');
+        $id = $_POST['id'];
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $endereco = $_POST['endereco'];
+        $bairro = $_POST['bairro'];
+        $cep = $_POST['cep'];
+        $id_cidade = $_POST['id_cidade'];
+
+        echo "<h1>Dados da cidade</h1>";
+        echo "Nome: $nome<br>";
+
+        $sql = "UPDATE pessoa SET nome = '$nome', pmail = '$email', endereço = '$endereco', bairro = '$bairro', cep = '$cep', id_cidade = '$id_cidade' WHERE id = $id";
+        echo $sql;
+
+        $result = mysqli_query($con,$sql);
+        if($result){
+            echo "<h2>Dados cadastrados com sucesso!!</h2>";
+        }else{
+            echo "<h2>Erro ao cadastrar!</h2>";
+            echo mysqli_error($con);
+        }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../Styles/style.css">
-</head>
-<body>
-    <h1>Alteração de Cliente</h1>
-    <?php
-    echo "<p>Id: $id</p>";
-    echo "<p>Nome: $nome</p>";
-    echo "<p>Email: $email</p>";
-    echo "<p>Endereço: $endereco</p>";
-    echo "<p>Bairro: $bairro</p>";
-    echo "<p>Id da Cidade: $id_cidade</p>";
-    echo "<p>CEP: $cep</p>";
-    $sql = "UPDATE pessoa SET
-                nome = '$nome',
-                email = '$email',
-                endereco = '$endereco',
-                bairro = '$bairro',
-                id_cidade = '$id_cidade',
-                cep = '$cep'
-            WHERE id = '$id'";
-    $result = mysqli_query($con, $sql);
-    if($result)
-        echo "Dados atualizados";
-    else
-        echo "Erro ao atualizar dados\n"
-        .mysqli_error($con);
-    ?>
-    <p></p>
-    <a href="ListarPessoa.php">Voltar</a>
-</body>
-</html>
